85a3809bd6b6dc4fe8700f276adc56a667558ba0
Avinish Rana <ranaavinish72@gmail.com>
2026-07-22 22:03:52 +0530

    refactor: remove unnecessary React fragment in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,80 +9,77 @@ const Header = ({ setPage }) => {
   const cartItems = useSelector((store) => store.cart.items);
 
   return (
-    <>
-      <header className="header">
-        <div className="header-left">
-          <div className="logo-container cursor-pointer" onClick={() => setPage("home")}>
-            <img
-              className="logo"
-              src="https://static.vecteezy.com/system/resources/thumbnails/024/284/682/small_2x/restaurant-logo-5-png.png"
-              alt="Logo"
-            />
-          </div>
+    <header className="header">
+      <div className="header-left">
+        <div className="logo-container cursor-pointer" onClick={() => setPage("home")}>
+          <img
+            className="logo"
+            src="https://static.vecteezy.com/system/resources/thumbnails/024/284/682/small_2x/restaurant-logo-5-png.png"
+            alt="Logo"
+          />
         </div>
-        <div className="search-wrapper">
-          <input type="text" placeholder="Search..." className="search-input" />
+      </div>
+      <div className="search-wrapper">
+        <input type="text" placeholder="Search..." className="search-input" />
+        <svg
+          className="search-icon-svg"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <circle cx="11" cy="11" r="8"></circle>
+          <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+        </svg>
+      </div>
+      <div className="header-right">
+        <nav className="nav-wrapper">
+          <ul className="nav-link-list">
+            <li className="Nav-elements" onClick={() => setPage("home")}>
+              Restaurants
+            </li>
+            <li className="Nav-elements" onClick={() => setPage("about")}>
+              About Us
+            </li>
+            <li className="Nav-elements" onClick={() => setPage("contact")}>
+              Contact Us
+            </li>
+          </ul>
+        </nav>
+        <div className="Nav-elements cart-nav">
           <svg
-            className="search-icon-svg"
+            className="cart-icon-svg"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            strokeWidth="2.5"
+            strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
           >
-            <circle cx="11" cy="11" r="8"></circle>
-            <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+            <circle cx="9" cy="21" r="1"></circle>
+            <circle cx="20" cy="21" r="1"></circle>
+            <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
           </svg>
+          <span onClick={() => {
+            setPage("cart");
+          }}>Cart ({cartItems.length})</span>
         </div>
-        <div className="header-right">
-          <nav className="nav-wrapper">
-            <ul className="nav-link-list">
-              <li className="Nav-elements" onClick={() => setPage("home")}>
-                Restaurants
-              </li>
-              <li className="Nav-elements" onClick={() => setPage("about")}>
-                About Us
-              </li>
-              <li className="Nav-elements" onClick={() => setPage("contact")}>
-                Contact Us
-              </li>
-            </ul>
-          </nav>
-          <div className="Nav-elements cart-nav">
-            <svg
-              className="cart-icon-svg"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <circle cx="9" cy="21" r="1"></circle>
-              <circle cx="20" cy="21" r="1"></circle>
-              <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
-            </svg>
-            <span onClick={() => {
-              setPage("cart");
-            }}>Cart ({cartItems.length})</span>
-          </div>
-          <button
-            className="Login-button"
-            onClick={() => {
-              btnNameReact === "Login"
-                ? setBtnNameReact("Logout")
-                : setBtnNameReact("Login");
-            }}
-          >
-            {btnNameReact}
-          </button>
-        </div>
-      </header>
-
-    </>
+        <button
+          className="Login-button"
+          onClick={() => {
+            btnNameReact === "Login"
+              ? setBtnNameReact("Logout")
+              : setBtnNameReact("Login");
+          }}
+        >
+          {btnNameReact}
+        </button>
+      </div>
+    </header>
   );
 };
 
